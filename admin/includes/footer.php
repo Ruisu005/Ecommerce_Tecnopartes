af30bce8e7c233259b4e1df050028d01bd7f51d3
Ruisu005 <luisacku55@gmail.com>
2024-07-09 16:54:52 -0600

    v.1.0

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Todos los Derechos Reservados</b>
+    </div>
+     <strong>Copyright &copy; 2024 <a href="">PC Componentes</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Todos los derechos reservados</b>
+      </div>
+      <strong>Copyright &copy; 2024 <a href="https://www.facebook.com/BermzISware">PC Compenentes</a></strong>
+    </div>
+</footer>
